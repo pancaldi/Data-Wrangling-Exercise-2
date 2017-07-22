4f25228dd635da2e6b8d46c4bb823714aef25741
Jacques Bikoundou <jaqo59@gmail.com>
2017-07-22 17:39:26 -0400

    Add files via upload

---
+++ dwex2.R
@@ -0,0 +1,32 @@
+# load and install packages in R
+
+library(readr)
+titanic_original_csv <- read_excel("C:/Users/jaqo_000/Desktop/titanic_original.csv")
+df <- titanic_original_csv
+View(titanic_original_csv)
+
+# port of embarkation
+
+for(i in 1:length(df$embarked)) {
+  if(is.na(df$embarked[i]) | is.null(trimws(df$embarked[i])) | trimws(df$embarked[i]) == "") {
+    df$embarked[i] <- "S"
+  }
+}
+
+# age of passengers
+
+meanAge <- mean(df$age, na.rm = TRUE)
+df <- mutate(titanic_original_csv, age = ifelse(is.na(age), meanAge, age)) 
+
+# lifeboat
+
+df <- mutate(titanic_original_csv, boat = ifelse(is.null(boat)) | is.na(boat) | trimws(boat) == "", NA, boat)
+
+# cabin
+
+df <- mutate(df, has_cabin_number = ifelse(is.null(cabin) | is.na(cabin) | trimws(cabin) == "", 0, 1))
+
+# write back to csv as titanic_clean.csv
+
+write.csv(titanic_original_csv, file = "titanic_clean_.csv")
+
